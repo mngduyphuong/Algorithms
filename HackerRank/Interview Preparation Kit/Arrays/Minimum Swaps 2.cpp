b62ae19ea51e1dafd73743db34a2d1b3481d98b2
Duy Phuong Nguyen <mngduyphuong@gmail.com>
2021-06-28 20:41:20 +0700

    upload

---
+++ HackerRank/Interview Preparation Kit/Arrays/Minimum Swaps 2.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumSwaps function below.
+int minimumSwaps(vector<int> arr) {
+    int result=0;
+    int size = arr.size();
+    int temp=0,real=0;
+    for(int i=0;i<size-1;i++)
+    {
+        while(arr[i]!=i+1)
+        {
+            real=arr[i];
+            temp=arr[real-1];
+            arr[real-1]=arr[i];
+            arr[i]=temp;
+            result++;
+            
+            // for(int j=0;j<size-1;j++)
+            // {
+            //     if(i+1 == arr[j])
+            //     {   temp=arr[i];
+            //         arr[i]=arr[j];
+            //         arr[j]=temp;
+            //         result++;
+            //     }
+            // }
+            
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = minimumSwaps(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
